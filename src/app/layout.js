aa4ab0ad5c2a32f223bd65c05582d0ea6338d00b
jaasonw <jayywong92@gmail.com>
2023-10-06 18:25:44 -0700

    initial commit

---
+++ src/app/layout.js
@@ -0,0 +1,15 @@
+import { Inter } from "next/font/google";
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "title",
+  description: "description",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
